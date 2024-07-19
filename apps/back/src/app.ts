4cd55361e3b0501234a60031c565bb3fedcfc4e7
m.leliakov <mixeyes82@gmail.com>
2024-07-19 15:30:13 +0200

    add clustering

--- apps/back/src/app.ts
+++ apps/back/src/app.ts
@@ -1,15 +1,75 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import compression from 'compression';
+import cors from 'cors';
+import winston from 'winston';
+import morgan from 'morgan';
+import os from 'os';
+import cluster from 'cluster';
 
 dotenv.config();
-
 const app: Application = express();
-const PORT = process.env['PORT'] || 3000;
+const PORT = process.env.PORT || 3000;
+const { combine, timestamp, json } = winston.format;
+const totalCPUs = os.cpus().length;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+if (cluster.isPrimary) {
+  console.log(`Number of CPUs is ${totalCPUs}`);
+  console.log(`Primary ${process.pid} is running`);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  // Fork workers.
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'http',
+    format: combine(
+      timestamp({
+        format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+      }),
+      json(),
+    ),
+    transports: [new winston.transports.Console()],
+    // transports: [new winston.transports.File({ dirname: '../logs', filename: 'log.json', maxsize: 100000, maxFiles: 5 })],
+  });
+
+  const morganMiddleware = morgan(
+    function (tokens, req, res) {
+      return JSON.stringify({
+        method: tokens.method(req, res),
+        url: tokens.url(req, res),
+        status: Number.parseFloat(tokens.status(req, res) || ''),
+        content_length: tokens.res(req, res, 'content-length'),
+        response_time: Number.parseFloat(tokens['response-time'](req, res) || ''),
+      });
+    },
+    {
+      stream: {
+        // Configure Morgan to use our custom logger with the http severity
+        write: (message) => {
+          const data = JSON.parse(message);
+          logger.http(`incoming-request`, data);
+        },
+      },
+    },
+  );
+
+  app.use(morganMiddleware);
+  app.use(compression());
+  app.use(cors());
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Worker process ${process.pid} is listening on port ${PORT}`);
+  });
+}
